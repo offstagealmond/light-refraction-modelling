ecd5bcf7ef80a74fb4057199612423c5ce9ee5e2
Almondd <33526139+offstagealmond@users.noreply.github.com>
2022-09-29 13:33:45 +0000

    added intersection coordinate calculation and angle of intersection and refraction calculation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sympy, turtle
+import sympy, turtle, math
 x,y,m,c = sympy.symbols("x y m c")
 
-laser = {"origin": [-150,0], "vector": [1,2.5]}
-glass_pane = {"origin": [5,-150], "endpoint": [5,150], "vector": None}
+laser = {"origin": [-150,0], "vector": [1,0.2]}
+glass_pane = {"origin": [5,-150], "endpoint": [5,150], "vector": None, "refractive index": 1.52}
 object_list = [laser, glass_pane]
 
 for object in object_list:
@@ -21,12 +21,32 @@ for object in object_list:
     y_intercept = sympy.solve(expr)[0]
     object["eq"] = sympy.Eq(y, object["gradient"]*x+y_intercept)
 
-print(sympy.solve([laser["eq"], glass_pane["eq"]], (x,y)))
+intersection_coordinates = sympy.solve([laser["eq"], glass_pane["eq"]], (x,y))
+intersection_coordinates = [intersection_coordinates[x], intersection_coordinates[y]]
+if glass_pane["gradient"] == "infinity":
+    normal = sympy.Eq(y, intersection_coordinates[1])
+    angle_of_intersection = sympy.atan(laser["gradient"])
+    angle_of_refraction = sympy.asin(sympy.sin(angle_of_intersection) / glass_pane["refractive index"])
+    print(angle_of_intersection, angle_of_refraction)
 
 print(laser)
 print(glass_pane)
 
 canvas = turtle.Screen()
+turtle_laser = turtle.Turtle()
+turtle_laser.hideturtle()
+turtle_laser.color("red")
+glass_pane_laser = turtle.Turtle()
+glass_pane_laser.penup()
+glass_pane_laser.color("gray")
+glass_pane_laser.setpos(glass_pane["origin"])
+glass_pane_laser.pendown()
+glass_pane_laser.setpos(glass_pane["endpoint"])
+turtle_laser.penup()
+turtle_laser.setpos(laser["origin"])
+turtle_laser.pendown()
+turtle_laser.pensize(3)
+turtle_laser.setpos(intersection_coordinates)
 # calculate m
 
 # calculate c by setting x to 0 and solving for y in y=mx+c
