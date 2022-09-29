8bbaac83f05017dd7163928f5c6211367c64dff4
Almondd <33526139+offstagealmond@users.noreply.github.com>
2022-09-29 13:48:03 +0000

    added refraction of laser through glass pane and visualisation on turtle

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ for object in object_list:
     # substitute in values given in the origin and gradient then solve for c
     expr = expr.subs([(y, object["origin"][1]), (x, object["origin"][0]), (m, object["gradient"])])
     y_intercept = sympy.solve(expr)[0]
+    object["y intercept"] = y_intercept
     object["eq"] = sympy.Eq(y, object["gradient"]*x+y_intercept)
 
 intersection_coordinates = sympy.solve([laser["eq"], glass_pane["eq"]], (x,y))
@@ -27,7 +28,9 @@ if glass_pane["gradient"] == "infinity":
     normal = sympy.Eq(y, intersection_coordinates[1])
     angle_of_intersection = sympy.atan(laser["gradient"])
     angle_of_refraction = sympy.asin(sympy.sin(angle_of_intersection) / glass_pane["refractive index"])
-    print(angle_of_intersection, angle_of_refraction)
+    gradient_of_refracted_laser = sympy.atan(angle_of_refraction)
+    refracted_laser_equation = sympy.Eq(y, gradient_of_refracted_laser*x+intersection_coordinates[1])
+    print(refracted_laser_equation)
 
 print(laser)
 print(glass_pane)
@@ -47,6 +50,7 @@ turtle_laser.setpos(laser["origin"])
 turtle_laser.pendown()
 turtle_laser.pensize(3)
 turtle_laser.setpos(intersection_coordinates)
+turtle_laser.setpos(1000, 1000*gradient_of_refracted_laser)
 # calculate m
 
 # calculate c by setting x to 0 and solving for y in y=mx+c
