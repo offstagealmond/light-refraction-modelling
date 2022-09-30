4c3542e24ae9b8d90ae5fa60f41561fc9a69a5ad
Almondd <33526139+offstagealmond@users.noreply.github.com>
2022-09-30 13:49:46 +0000

    added support for sloped interfaces and fixed gradient mathematics

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
 import sympy, turtle, math
 x,y,m,c = sympy.symbols("x y m c")
 
-laser = {"origin": [-150,0], "vector": [1,0.2]}
-glass_pane = {"origin": [5,-150], "endpoint": [5,150], "vector": None, "refractive index": 1.52}
+laser = {"origin": [-100,0], "vector": [5,1], "current medium ri": 1}
+glass_pane = {"origin": [0,-150], "endpoint": [0,150], "vector": None, "refractive index": 1.52}
 object_list = [laser, glass_pane]
+
+intersection_coordinates = []
+gradient_of_refracted_laser = None
 
 for object in object_list:
     # find the gradient using the direction vector
     try:
         if object["vector"] == None:
-            glass_pane["vector"] = [(object["origin"][0] - object["endpoint"][0]), (object["origin"][1] - object["endpoint"][1])]
+            object["vector"] = [(object["origin"][0] - object["endpoint"][0]), (object["origin"][1] - object["endpoint"][1])]
         object["gradient"] = object["vector"][1] / object["vector"][0]
-    except ZeroDivisionError:
+    except ZeroDivisionError: # if the gradient is infinity
         object["gradient"] = "infinity"
         object["eq"] = sympy.Eq(x, object["origin"][0])
-        continue
-    expr = sympy.Eq(y, m*x+c)
-    # substitute in values given in the origin and gradient then solve for c
-    expr = expr.subs([(y, object["origin"][1]), (x, object["origin"][0]), (m, object["gradient"])])
-    y_intercept = sympy.solve(expr)[0]
-    object["y intercept"] = y_intercept
-    object["eq"] = sympy.Eq(y, object["gradient"]*x+y_intercept)
+    else:
+        expr = sympy.Eq(y, m*x+c)
+        # substitute in values given in the origin and gradient then solve for c
+        expr = expr.subs([(y, object["origin"][1]), (x, object["origin"][0]), (m, object["gradient"])])
+        y_intercept = sympy.solve(expr)[0]
+        object["y intercept"] = y_intercept
+        object["eq"] = sympy.Eq(y, object["gradient"]*x+y_intercept) # form the line equation
+    finally:
+        if object == laser:
+            continue
+        solve_for_coordinates = sympy.solve([laser["eq"], object["eq"]], (x,y))
+        solve_for_coordinates = [solve_for_coordinates[x], solve_for_coordinates[y]]
+        intersection_coordinates.append(solve_for_coordinates)
 
-intersection_coordinates = sympy.solve([laser["eq"], glass_pane["eq"]], (x,y))
-intersection_coordinates = [intersection_coordinates[x], intersection_coordinates[y]]
-if glass_pane["gradient"] == "infinity":
-    normal = sympy.Eq(y, intersection_coordinates[1])
-    angle_of_intersection = sympy.atan(laser["gradient"])
-    angle_of_refraction = sympy.asin(sympy.sin(angle_of_intersection) / glass_pane["refractive index"])
-    gradient_of_refracted_laser = sympy.atan(angle_of_refraction)
-    refracted_laser_equation = sympy.Eq(y, gradient_of_refracted_laser*x+intersection_coordinates[1])
+for count, object in enumerate(object_list):
+    count-=1
+    if object == laser:
+        continue
+    if object["gradient"] == "infinity":
+        normal = sympy.Eq(y, intersection_coordinates[count][1])
+        angle_of_intersection = sympy.atan(laser["gradient"])
+        angle_of_refraction = sympy.asin((laser["current medium ri"]*sympy.sin(angle_of_intersection)) / object["refractive index"])
+        gradient_of_refracted_laser = sympy.tan(angle_of_refraction)
+        refracted_laser_equation = sympy.Eq(y, gradient_of_refracted_laser*x+intersection_coordinates[0][1])
+    else:
+        normal_gradient = (-1/object["gradient"])
+        normal = sympy.Eq(y, normal_gradient*x+intersection_coordinates[count][1])
+        difference_of_gradients = laser["gradient"] - normal_gradient
+        angle_of_intersection = sympy.atan(difference_of_gradients)
+        angle_of_refraction = sympy.asin((laser["current medium ri"]*sympy.sin(angle_of_intersection)) / object["refractive index"])
+        gradient_of_refracted_laser = sympy.tan(angle_of_refraction)
+        refracted_laser_equation = sympy.Eq(y, gradient_of_refracted_laser*x+intersection_coordinates[0][1])
+    print(refracted_laser_equation)
+    print(angle_of_intersection, angle_of_refraction)
+    print(laser["gradient"], gradient_of_refracted_laser)
     print(refracted_laser_equation)
 
-print(laser)
-print(glass_pane)
 
 canvas = turtle.Screen()
 turtle_laser = turtle.Turtle()
@@ -49,7 +69,7 @@ turtle_laser.penup()
 turtle_laser.setpos(laser["origin"])
 turtle_laser.pendown()
 turtle_laser.pensize(3)
-turtle_laser.setpos(intersection_coordinates)
+turtle_laser.setpos(intersection_coordinates[0])
 turtle_laser.setpos(1000, 1000*gradient_of_refracted_laser)
 # calculate m
 
